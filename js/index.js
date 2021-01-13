c4ea799945689758b7e2c2ff0528fe256c27f42c
Casey Dietz <casey.dietz@ihsmarkit.com>
2021-01-13 15:44:22 -0700

    MVP complete

--- js/index.js
+++ js/index.js
@@ -41,6 +41,8 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
+// My code starts here
+
 let navHeader = document.querySelectorAll('a');
 navHeader[0].textContent = 'Services'
 navHeader[1].textContent = 'Product'
@@ -68,3 +70,27 @@ let featuresContent = document.querySelectorAll('.top-content p')
 featuresContent[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
 
 featuresContent[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+let middleImg = document.querySelector('.middle-img')
+middleImg.src = 'img/mid-page-accent.jpg';
+
+let bottomContentHeader = document.querySelectorAll('.bottom-content h4')
+bottomContentHeader[0].textContent = 'Services'
+bottomContentHeader[1].textContent = 'Product'
+bottomContentHeader[2].textContent = 'Vision'
+
+let bottomContentParagraph = document.querySelectorAll('.bottom-content p')
+bottomContentParagraph[0].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+bottomContentParagraph[1].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+bottomContentParagraph[2].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+let sectionContact = document.querySelector('.contact h4')
+sectionContact.textContent = 'Contact'
+
+let sectionContactParagraph = document.querySelectorAll('.contact p')
+sectionContactParagraph[0].textContent = '123 Way 456 Street Somewhere, USA'
+sectionContactParagraph[1].textContent = '1 (888) 888-8888'
+sectionContactParagraph[2].textContent = 'sales@greatidea.io'
+
+let footerSection = document.querySelector('footer p')
+footerSection.textContent = 'Copyright Great Idea! 2018'
